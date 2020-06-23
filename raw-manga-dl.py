1490ae64412111e84b785e500865022209023c35
vtbk <25325895+vtbk@users.noreply.github.com>
2020-06-23 17:24:59 +0200

    Add CLI functionality for specifying storage method

--- raw-manga-dl.py
+++ raw-manga-dl.py
@@ -6,11 +6,11 @@ from Storage.ZipStorage import ZipStorage
 import argparse
 import os
 
-#Testing purposes only
 #TO-DO clean this up, expand args, move services container elsewhere, and so on.. 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='downloads manga chapters')
     parser.add_argument('--download')
+    parser.add_argument('-o', default="folder")
     parser.add_argument('path')
     args = parser.parse_args()
     solver = PtImgSolver()
@@ -18,8 +18,12 @@ if __name__ == "__main__":
         ComicTrail(solver),
         StoriaTakeShobo(solver)
     ]
+    storage_options = {
+        "folder":   lambda path: FolderStorage(path),
+        "zip":      lambda path: ZipStorage(path)
+    }
     if args.download:
-        storage = FolderStorage(args.path)
+        storage = storage_options.get(args.o)(args.path)
         for service in services:
             if service.supports_url(args.download):
                 print("Found matching service for requested chapter url")
